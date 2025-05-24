105583d5ed3da44637c53ff68c6b13806a243395
Etn12345 <156725331+Etn12345@users.noreply.github.com>
2025-05-24 13:55:03 -0400

    Button

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,8 +1,24 @@
+
+"use client";
+
 import Image from "next/image";
 
+function MyButton() {
+  function handleClick() {
+    alert("Egret");
+  }
+  
+  return (
+    <button onClick={handleClick}>
+      I'm a button
+    </button>
+  );
+}
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <MyButton />
+
 
     </div>
   );
